609f1e071329044a0a8fd921b81046ab05a74294
FedUtility <frmfernando@globo.com>
2014-09-25 00:15:36 -0300

    Fixed

--- FedAllChampionsUtility/Properties/AssemblyInfo.cs
+++ FedAllChampionsUtility/Properties/AssemblyInfo.cs
@@ -2,10 +2,10 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Informações gerais sobre um assembly são controladas através do seguinte 
-// conjunto de atributos. Altere o valor destes atributos para modificar a informação
-// associada a um assembly.
-[assembly: AssemblyTitle("FedAllChampionsUtility")]
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FedAllChampionsUtility.Properties")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -14,20 +14,20 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Definir ComVisible como false torna os tipos neste assembly não visíveis 
-// para componentes COM. Caso precise acessar um tipo neste assembly a partir de 
-// COM, defina o atributo ComVisible como true nesse tipo.
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// O GUID a seguir é para a ID da typelib se este projeto for exposto para COM
-[assembly: Guid("9cf28e53-530d-4536-9eb7-04cf2421d17c")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f7fdbf10-4c85-4457-8811-274c0de40a26")]
 
-// Informações de Versão para um assembly consistem nos quatro valores a seguir:
+// Version information for an assembly consists of the following four values:
 //
-//      Versão Principal
-//      Versão Secundária 
-//      Número da Versão
-//      Revisão
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
 //
-[assembly: AssemblyVersion("1.0.1.2")]
-[assembly: AssemblyFileVersion("1.0.1.2")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
