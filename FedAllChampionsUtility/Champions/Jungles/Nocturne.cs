186fdd0382849e16e271902329657da82319510e
FedUtility <frmfernando@globo.com>
2014-09-29 16:18:23 -0300

    test

--- FedAllChampionsUtility/Champions/Jungles/Nocturne.cs
+++ FedAllChampionsUtility/Champions/Jungles/Nocturne.cs
@@ -187,6 +187,7 @@ namespace FedAllChampionsUtility
             {
                 if (ObjectManager.Player.Distance(rTarget) > 1300 && EnemmylowHP(Program.Menu.Item("HPR").GetValue<Slider>().Value, GetRRange()))
                 {
+                    R.Cast();
                     R.CastOnUnit(rTarget, Packets());
                 }
             }            
