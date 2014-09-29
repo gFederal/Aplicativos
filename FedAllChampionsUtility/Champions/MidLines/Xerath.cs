20390864c80e4af6c84eda1b6d0536b3fc9f58ae
FedUtility <frmfernando@globo.com>
2014-09-29 19:13:44 -0300

    teste

--- FedAllChampionsUtility/Champions/MidLines/Xerath.cs
+++ FedAllChampionsUtility/Champions/MidLines/Xerath.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿#region
+
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
-
 using Color = System.Drawing.Color;
+
+#endregion
 
 namespace FedAllChampionsUtility
 {
