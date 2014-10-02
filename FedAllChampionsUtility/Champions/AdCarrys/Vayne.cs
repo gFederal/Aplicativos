7fbc2c07ffdea9c1b3cc6bac36d9b91780c54c95
FedUtility <frmfernando@globo.com>
2014-10-02 02:16:50 -0300

    Update

--- FedAllChampionsUtility/Champions/AdCarrys/Vayne.cs
+++ FedAllChampionsUtility/Champions/AdCarrys/Vayne.cs
@@ -15,7 +15,11 @@ namespace FedAllChampionsUtility
 {
     class Vayne : Champion
     {        
-        public Spell E, QE, Q, R;        
+        public Spell E, QE, Q, R;
+
+        public static string[] interrupt;
+        public static string[] notarget;
+        public static string[] gapcloser;
 
         public Vayne()
         {
@@ -25,6 +29,8 @@ namespace FedAllChampionsUtility
             Drawing.OnDraw += Drawing_OnDraw;            
             Game.OnGameUpdate += Game_OnGameUpdate;
             Orbwalking.AfterAttack += Orbwalking_AfterAttack;
+            Obj_AI_Base.OnProcessSpellCast += OnProcessSpell;
+
             PluginLoaded();
         }
 
@@ -42,57 +48,95 @@ namespace FedAllChampionsUtility
         
         private void LoadMenu()
          {
-
              Program.Menu.AddSubMenu(new Menu("TeamFight", "TeamFight"));
              Program.Menu.SubMenu("TeamFight").AddItem(new MenuItem("UseQC", "Use Q").SetValue(true));
              Program.Menu.SubMenu("TeamFight").AddItem(new MenuItem("UseEC", "Use E").SetValue(true));
              Program.Menu.SubMenu("TeamFight").AddItem(new MenuItem("UseQEC", "Use QE if not in E range").SetValue(true));
              Program.Menu.SubMenu("TeamFight").AddItem(new MenuItem("UseET", "Use E (Toggle)").SetValue(new KeyBind("T".ToCharArray()[0], KeyBindType.Toggle)));
 
+             Program.Menu.AddSubMenu(new Menu("Harass", "Harass"));
+             Program.Menu.SubMenu("Harass").AddItem(new MenuItem("UseQH", "Use Q").SetValue(true));             
+
              Program.Menu.AddSubMenu(new Menu("Passive", "Passive"));
-             Program.Menu.SubMenu("Passive").AddItem(new MenuItem("UseEInterrupt", "Use E To Interrupt").SetValue(true));
-             Program.Menu.SubMenu("Passive").AddItem(new MenuItem("PushDistance", "E Push Distance").SetValue(new Slider(450, 475, 300)));
+             Program.Menu.SubMenu("Passive").AddItem(new MenuItem("AntiGP", "Use AntiGapcloser").SetValue(true));
+             Program.Menu.SubMenu("Passive").AddItem(new MenuItem("UseEInterrupt", "Interrupt Spells").SetValue(true));
+             Program.Menu.SubMenu("Passive").AddItem(new MenuItem("QDefense", "Use Q Defensive").SetValue(true));
+             Program.Menu.SubMenu("Passive").AddItem(new MenuItem("AdvE", "Test Advense E").SetValue(false)); 
+             Program.Menu.SubMenu("Passive").AddItem(new MenuItem("PushDistance", "E Push Distance").SetValue(new Slider(450, 475, 400)));
              Program.Menu.SubMenu("Passive").AddItem(new MenuItem("AutoR", "Auto R").SetValue(true));
-             Program.Menu.SubMenu("Passive").AddItem(new MenuItem("MinR", "Min Enemys for (R)").SetValue<Slider>(new Slider(3, 1, 5)));              
+             Program.Menu.SubMenu("Passive").AddItem(new MenuItem("MinR", "Min Enemys for (R)").SetValue<Slider>(new Slider(3, 1, 5)));
+
+             Program.Menu.AddSubMenu(new Menu("Use Items", "Items"));
+             Program.Menu.SubMenu("Items").AddItem(new MenuItem("Botrk", "Use BOTRK").SetValue(true));
+             Program.Menu.SubMenu("Items").AddItem(new MenuItem("Youmuu", "Use Youmuu").SetValue(true));
+             Program.Menu.SubMenu("Items").AddItem(new MenuItem("OwnHPercBotrk", "Min Own HP % Botrk").SetValue(new Slider(50, 1, 100)));
+             Program.Menu.SubMenu("Items").AddItem(new MenuItem("EnHPercBotrk", "Min Enemy HP % Botrk").SetValue(new Slider(50, 1, 100)));
+             Program.Menu.SubMenu("Items").AddItem(new MenuItem("ItInMix", "Use Items In Harass").SetValue(false));
 
              Program.Menu.AddSubMenu(new Menu("Drawing", "Drawing"));
              Program.Menu.SubMenu("Drawing").AddItem(new MenuItem("Draw_Disabled", "Disable All").SetValue(false));
              Program.Menu.SubMenu("Drawing").AddItem(new MenuItem("Draw_Q", "Draw Q + Attack").SetValue(true));             
              Program.Menu.SubMenu("Drawing").AddItem(new MenuItem("Draw_E", "Draw E").SetValue(false));
-             
+
+             Program.Menu.AddSubMenu(new Menu("Gapcloser", "gap"));
+             Program.Menu.AddSubMenu(new Menu("Gapcloser 2", "gap2"));
+             Program.Menu.AddSubMenu(new Menu("Interrupts", "int"));
+             GPIntmenuCreate();
          }
 
         public void Game_OnGameUpdate(EventArgs args)
         {
-            if ((!E.IsReady()) ||
-                ((Program.Orbwalker.ActiveMode != Orbwalking.OrbwalkingMode.Combo || !Program.Menu.Item("UseQEC").GetValue<bool>()) &&
-                 !Program.Menu.Item("UseET").GetValue<KeyBind>().Active))
-            {
-                return;
-            }
-
             var normalTarget = SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Physical);
-            var qeTarget = SimpleTs.GetTarget(200 + E.Range, SimpleTs.DamageType.Physical);
-            if (normalTarget.IsValid)
-            {
-                if (IsStunable(normalTarget))
+            var qeTarget = SimpleTs.GetTarget(200 + E.Range, SimpleTs.DamageType.Physical);            
+
+            if (Program.Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo || Program.Menu.Item("UseET").GetValue<KeyBind>().Active)
+            {               
+
+                if (normalTarget.IsValid)
                 {
-                    E.Cast(normalTarget);
+                    if (IsStunable(normalTarget) && E.IsReady())
+                    {
+                        E.Cast(normalTarget, Packets());
+                    }
                 }
-            }
-            else if (qeTarget.IsValid)
-            {
-                if (!IsStunable(qeTarget) || !Program.Menu.Item("UseQEC").GetValue<bool>() || !Q.IsReady())
+                else if (qeTarget.IsValid)
                 {
-                    return;
+                    if (!IsStunable(qeTarget) || !Program.Menu.Item("UseQEC").GetValue<bool>() || !Q.IsReady() || !E.IsReady())
+                    {
+                        return;
+                    }
+                    Q.Cast(qeTarget.ServerPosition, Packets());
+                    Utility.DelayAction.Add(250, () => E.Cast(qeTarget, Packets()));
                 }
-                Q.Cast(qeTarget);
-                Utility.DelayAction.Add(250, () => E.Cast(qeTarget));
             }
 
-            if (Program.Menu.Item("AutoR").GetValue<bool>())
+            if (Program.Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo && Program.Menu.Item("AutoR").GetValue<bool>())
             {
                 AutoUlt();
+            }
+
+            if (Program.Menu.Item("QDefense").GetValue<bool>())
+            {
+                QDefense();
+            }
+
+            if (Program.Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo || (Program.Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed && Program.Menu.Item("ItInMix").GetValue<bool>()))
+            {
+                useItems(normalTarget);
+            }
+        }
+
+        private void useItems(Obj_AI_Hero tar)
+        {
+            float OwnH = getPlHPer();
+            if (Program.Menu.Item("Botrk").GetValue<bool>() && (Program.Menu.Item("OwnHPercBotrk").GetValue<Slider>().Value <= OwnH) && ((Program.Menu.Item("EnHPercBotrk").GetValue<Slider>().Value <= getEnH(tar))))
+            {
+                useItem(3144, tar);
+                useItem(3153, tar);
+            }
+            if (Program.Menu.Item("Youmuu").GetValue<bool>())
+            {
+                useItem(3142);
             }
         }
 
@@ -105,6 +149,18 @@ namespace FedAllChampionsUtility
                 R.Cast();
             }
 
+        }        
+
+        private void QDefense()
+        {
+            foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>())
+            {
+                if (enemy.IsValidTarget(E.Range) && enemy.Distance(ObjectManager.Player) <= enemy.BoundingRadius + enemy.AttackRange + ObjectManager.Player.BoundingRadius && enemy.IsMelee() && !enemy.IsDead)
+                {
+                    var pos = ObjectManager.Player.ServerPosition.To2D().Extend(enemy.Position.To2D(), -300).To3D();
+                    Q.Cast(pos, Packets());                    
+                }
+            }
         }
 
         private void Drawing_OnDraw(EventArgs args)
@@ -122,32 +178,15 @@ namespace FedAllChampionsUtility
                 if (E.Level > 0)
                     Utility.DrawCircle(ObjectManager.Player.Position, E.Range+107, E.IsReady() ? Color.Green : Color.Red);
             
-        }
-
-        public void AntiGapcloser_OnEnemyGapcloser(ActiveGapcloser gapcloser)
-        {
-            if (E.IsReady() && gapcloser.Sender.IsValidTarget(E.Range))
-            {
-                E.CastOnUnit(gapcloser.Sender);
-            }
-        }
-
-        public void Interrupter_OnPossibleToInterrupt(Obj_AI_Base unit, InterruptableSpell spell)
-        {
-            if (Program.Menu.Item("UseEInterrupt").GetValue<bool>() && unit.IsValidTarget(550f))
-            {
-                E.Cast(unit);
-            }
-        }
-
+        }        
         public void Orbwalking_AfterAttack(Obj_AI_Base unit, Obj_AI_Base target)
         {
-            if (unit.IsMe && Program.Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo && Program.Menu.Item("UseQC").GetValue<bool>())
+            if (unit.IsMe && (Program.Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo && Program.Menu.Item("UseQC").GetValue<bool>() ||
+                              Program.Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed && Program.Menu.Item("UseQH").GetValue<bool>()))
             {
                 CastTumble((Obj_AI_Hero)target);
             }
         }
-
         public bool IsStunable(Obj_AI_Hero enemy)
         {
             var prediction =
@@ -162,17 +201,35 @@ namespace FedAllChampionsUtility
 
             for (var i = 0; i < Program.Menu.Item("PushDistance").GetValue<Slider>().Value; i += (int)enemy.BoundingRadius)
             {
-                if (
-                    NavMesh.GetCollisionFlags(
-                        prediction.UnitPosition.To2D().Extend(ObjectManager.Player.ServerPosition.To2D(), -i).To3D())
-                        .HasFlag(CollisionFlags.Wall))
+                if (Program.Menu.Item("AdvE").GetValue<bool>())
                 {
-                    return true;
+                    if (
+                        NavMesh.GetCollisionFlags(
+                            prediction.UnitPosition.To2D().Extend(ObjectManager.Player.ServerPosition.To2D(), -i).To3D())
+                            .HasFlag(CollisionFlags.Wall) ||
+                        NavMesh.GetCollisionFlags(
+                            prediction.UnitPosition.To2D().Extend(ObjectManager.Player.ServerPosition.To2D(), -i).To3D())
+                            .HasFlag(CollisionFlags.Building) ||
+                        NavMesh.GetCollisionFlags(
+                            prediction.UnitPosition.To2D().Extend(ObjectManager.Player.ServerPosition.To2D(), -i).To3D())
+                            .HasFlag(CollisionFlags.Prop))
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    if (
+                        NavMesh.GetCollisionFlags(
+                            prediction.UnitPosition.To2D().Extend(ObjectManager.Player.ServerPosition.To2D(), -i).To3D())
+                            .HasFlag(CollisionFlags.Wall))
+                    {
+                        return true;
+                    }
                 }
             }
             return false;
         }
-
         public void CastTumble(Obj_AI_Hero target)
         {
             var posAfterTumble =
@@ -180,9 +237,84 @@ namespace FedAllChampionsUtility
             var distanceAfterTumble = Vector3.DistanceSquared(posAfterTumble, target.ServerPosition);
             if (distanceAfterTumble < 550 * 550 && distanceAfterTumble > 100 * 100)
             {
-                Q.Cast(Game.CursorPos);
+                Q.Cast(Game.CursorPos, Packets());
             }
-        }        
+        }
+        private void GPIntmenuCreate()
+        {
+            gapcloser = new[]
+            {
+                "AkaliShadowDance", "Headbutt", "DianaTeleport", "IreliaGatotsu", "JaxLeapStrike", "JayceToTheSkies",
+                "MaokaiUnstableGrowth", "MonkeyKingNimbus", "Pantheon_LeapBash", "PoppyHeroicCharge", "QuinnE",
+                "XenZhaoSweep", "blindmonkqtwo", "FizzPiercingStrike", "RengarLeap"
+            };
+            notarget = new[]
+            {
+                "AatroxQ", "GragasE", "GravesMove", "HecarimUlt", "JarvanIVDragonStrike", "JarvanIVCataclysm", "KhazixE",
+                "khazixelong", "LeblancSlide", "LeblancSlideM", "LeonaZenithBlade", "UFSlash", "RenektonSliceAndDice",
+                "SejuaniArcticAssault", "ShenShadowDash", "RocketJump", "slashCast"
+            };
+            interrupt = new[]
+            {
+                "KatarinaR", "GalioIdolOfDurand", "Crowstorm", "Drain", "AbsoluteZero", "ShenStandUnited", "UrgotSwap2",
+                "AlZaharNetherGrasp", "FallenOne", "Pantheon_GrandSkyfall_Jump", "VarusQ", "CaitlynAceintheHole",
+                "MissFortuneBulletTime", "InfiniteDuress", "LucianR"
+            };
+            for (int i = 0; i < gapcloser.Length; i++)
+            {
+                Program.Menu.SubMenu("gap").AddItem(new MenuItem(gapcloser[i], gapcloser[i])).SetValue(true);
+            }
+            for (int i = 0; i < notarget.Length; i++)
+            {
+                Program.Menu.SubMenu("gap2").AddItem(new MenuItem(notarget[i], notarget[i])).SetValue(true);
+            }
+            for (int i = 0; i < interrupt.Length; i++)
+            {
+                Program.Menu.SubMenu("int").AddItem(new MenuItem(interrupt[i], interrupt[i])).SetValue(true);
+            }
+        }
+        private bool isEn(String opt)
+        {
+            return Program.Menu.Item(opt).GetValue<bool>();
+        }
+        private void OnProcessSpell(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+        {
+            String spellName = args.SData.Name;
+            //Interrupts
+            if (isEn(spellName) && sender.IsValidTarget(550f) && isEn("UseEInterrupt"))
+            {
+                E.Cast((Obj_AI_Hero)sender, Packets());
+            }
+            //Targeted GapClosers
+            if (isEn(spellName) && sender.IsValidTarget(550f) && isEn("AntiGP") && gapcloser.Any(str => str.Contains(args.SData.Name))
+                && args.Target.IsMe)
+            {
+                E.Cast((Obj_AI_Hero)sender, Packets());
+            }
+            //NonTargeted GP
+            if (isEn(spellName) && sender.IsValidTarget(550f) && isEn("AntiGP") && notarget.Any(str => str.Contains(args.SData.Name))
+                && ObjectManager.Player.Distance(args.End) <= 320f)
+            {
+                E.Cast((Obj_AI_Hero)sender, Packets());
+            }
+        }
+        private void useItem(int id, Obj_AI_Hero target = null)
+        {
+            if (Items.HasItem(id) && Items.CanUseItem(id))
+            {
+                Items.UseItem(id, target);
+            }
+        }
+        private float getEnH(Obj_AI_Hero target)
+        {
+            float h = (target.Health / target.MaxHealth) * 100;
+            return h;
+        }
+        private float getPlHPer()
+        {
+            float h = (ObjectManager.Player.Health / ObjectManager.Player.MaxHealth) * 100;
+            return h;
+        }
         
     }
 }
